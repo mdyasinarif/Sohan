6f2d76bc6d0604485b5d39a892312802796db56c
Md Yasin Arif <mdyasinarifidb49@gmail.com>
2018-09-28 03:52:41 +0600

    wed updet

---
+++ style.c
@@ -0,0 +1,15 @@
+*{margin:0;padding:0;outline:0}
+body{font-family:AdorshoLipi;Bangla;CharuChandan3D; Arial; sans-serif;font-size:14px;height:10px;color:#000;}
+.templete{width:960px;margin:0 auto;}
+.headersection{}
+.headersection h2{}
+.navsection{}
+.contain{}
+.ma{}
+.ma p{}
+.mb{}
+.mb p{}
+.mc{}
+.mc p{}
+.footer{}
+.footer p{}
